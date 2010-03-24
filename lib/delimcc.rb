f7c5f77bf391cf3c7cbe8abef61d9d1aa6748c13
Steffen <steffen.siering@gmail.com>
2010-03-24 11:21:33 +0100

    little "cleanup" after using reek

--- lib/delimcc.rb
+++ lib/delimcc.rb
@@ -9,32 +9,32 @@ module DelimCC
         end
     end
 
-    def metacc_set(&m)
-        Thread.current["__meta_cc__"] = m
+    def metacc_set(&meta_cont)
+        Thread.current["__meta_cc__"] = meta_cont
     end
 
 
     def abortcc(&blk)
-        v = blk.call
-        metacc_get.call v
+        value = blk.call
+        metacc_get.call value
     end
 
     def reset(&blk)
-        callcc do |k|
-            m = metacc_get
-            metacc_set {|r|
-                metacc_set &m
-                k.call r
+        callcc do |cont|
+            meta_cont = metacc_get
+            metacc_set {|value|
+                metacc_set &meta_cont
+                cont.call value
             }
             abortcc &blk
         end
     end
 
     def shift(&blk)
-        callcc do |k|
+        callcc do |cont|
             abortcc do
-                blk.call proc {|x|
-                    reset { k.call x }
+                blk.call proc {|value|
+                    reset { cont.call value }
                 }
             end
         end
